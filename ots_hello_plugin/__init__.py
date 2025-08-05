9bb60d15700bd55840a03244d39b13aac20015b5
Giang <h.givngnguyen@gmail.com>
2025-08-05 15:34:20 -0400

    just trying things atp

--- ots_hello_plugin/__init__.py
+++ ots_hello_plugin/__init__.py
@@ -1,5 +1,5 @@
 # These version placeholders will be replaced later during substitution.
-from .app import Blueprint
+from .app import blueprint
 
 __version__ = "1.0.0"
 __version_tuple__ = (1, 0, 0)
