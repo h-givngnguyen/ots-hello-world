17b20f5faa0f40faa88b24ba736897f6452b2a17
Giang <h.givngnguyen@gmail.com>
2025-08-05 21:24:22 -0400

    will this work

--- ots_hello_plugin/__init__.py
+++ ots_hello_plugin/__init__.py
@@ -1,3 +1,4 @@
 # These version placeholders will be replaced later during substitution.
+from .app import blueprint
 __version__ = "0.0.0-post.25+030f7b3"
 __version_tuple__ = (0, 0, 0, "post", 25, "030f7b3")
